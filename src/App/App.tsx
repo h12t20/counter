80620acdc2cdbaeb1ce744a8ae75c622f7dac55a
h12t20 <sergdiamond@ymail.com>
2023-07-05 20:33:47 +0300

    include reducer, change logic

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -30,23 +30,23 @@ export const reducer = (state: StateType, action: ReducerType): StateType => {
         case 'MIN_TITLE': {
             return {
                 ...state,
-                inputMinTitle: +action.payload.value<=state.inputMaxTitle &&
-                +action.payload.value>=-1?
-                    +action.payload.value:state.inputMinTitle,
+                inputMinTitle: +action.payload.value <= state.inputMaxTitle &&
+                +action.payload.value >= -1 ?
+                    +action.payload.value : state.inputMinTitle,
                 disable: false,
-                error: +action.payload.value>=state.inputMaxTitle?'Err1':
-                    +action.payload.value<0? 'Err2': state.value>=state.maxValue? state.value.toString(): INF_MESSAGE
+                error: +action.payload.value >= state.inputMaxTitle ? 'Err1' :
+                    +action.payload.value < 0 ? 'Err2' : state.value >= state.maxValue ? state.value.toString() : INF_MESSAGE
             }
         }
         case 'MAX_TITLE': {
             return {
                 ...state,
-                inputMaxTitle: +action.payload.value>=state.inputMinTitle &&
-                +action.payload.value>=1?
-                    +action.payload.value:state.inputMaxTitle,
+                inputMaxTitle: +action.payload.value >= state.inputMinTitle &&
+                +action.payload.value >= 1 ?
+                    +action.payload.value : state.inputMaxTitle,
                 disable: false,
-                error: +action.payload.value<=state.inputMinTitle?'Err1':
-                     state.value>=state.maxValue? state.value.toString(): INF_MESSAGE
+                error: +action.payload.value <= state.inputMinTitle ? 'Err1' :
+                    state.value >= state.maxValue ? state.value.toString() : INF_MESSAGE
             }
         }
         case 'SET_HANDLER': {
@@ -62,34 +62,35 @@ export const reducer = (state: StateType, action: ReducerType): StateType => {
         case 'INC_HANDLER': {
             return {
                 ...state,
-                value:state.value<state.maxValue?state.value+1:state.value,
-                error: !state.error && state.value>=state.maxValue-1? state.value.toString(): state.error
+                value: state.value < state.maxValue ? state.value + 1 : state.value,
+                error: !state.error && state.value >= state.maxValue - 1 ? state.value.toString() : state.error
             }
         }
-        default: return state
+        default:
+            return state
     }
 }
-type ReducerType=ResetACType | MinTitleChangeACType | MaxTitleChangeACType | SetHandlerACType | IncHandlerACType
-type ResetACType=ReturnType<typeof resetAC>
-type MinTitleChangeACType=ReturnType<typeof minTitleChangeAC>
-type MaxTitleChangeACType=ReturnType<typeof maxTitleChangeAC>
-type SetHandlerACType=ReturnType<typeof setHandlerAC>
-type IncHandlerACType=ReturnType<typeof incHandlerAC>
+type ReducerType = ResetACType | MinTitleChangeACType | MaxTitleChangeACType | SetHandlerACType | IncHandlerACType
+type ResetACType = ReturnType<typeof resetAC>
+type MinTitleChangeACType = ReturnType<typeof minTitleChangeAC>
+type MaxTitleChangeACType = ReturnType<typeof maxTitleChangeAC>
+type SetHandlerACType = ReturnType<typeof setHandlerAC>
+type IncHandlerACType = ReturnType<typeof incHandlerAC>
 export const resetAC = () => {
     return {
         type: 'RESET'
-        } as const
+    } as const
 }
 export const minTitleChangeAC = (value: string) => {
     return {
         type: 'MIN_TITLE',
-        payload: { value }
+        payload: {value}
     } as const
 }
 export const maxTitleChangeAC = (value: string) => {
     return {
         type: 'MAX_TITLE',
-        payload: { value }
+        payload: {value}
     } as const
 }
 export const setHandlerAC = () => {
@@ -102,65 +103,66 @@ export const incHandlerAC = () => {
         type: 'INC_HANDLER'
     } as const
 }
-    function App() {
 
-        const storageMinValueAsString = localStorage.getItem('counterMinValue');
-        const storageMinValue = storageMinValueAsString ? +storageMinValueAsString : 0;
-        const storageMaxValueAsString = localStorage.getItem('counterMaxValue');
-        const storageMaxValue = storageMaxValueAsString ? +storageMaxValueAsString : 10;
-        const storageValueAsString = localStorage.getItem('counterValue');
-        const initialState: StateType = {
-            value: storageValueAsString && storageMinValueAsString ?
-                Math.max(+storageValueAsString, +storageMinValueAsString) : storageValueAsString ?
-                    +storageValueAsString : storageMinValueAsString ? +storageMinValueAsString : 0,
-            minValue: storageMinValue,
-            maxValue: storageMaxValue,
-            error: storageValueAsString && storageMaxValueAsString && (+storageValueAsString >=
-                +storageMaxValueAsString)? storageValueAsString:'',
-            inputMinTitle: storageMinValue,
-            inputMaxTitle: storageMaxValue,
-            disable: true
-        }
-        const [state, dispatchState] = useReducer(reducer, initialState)
+function App() {
 
-        useEffect(() => localStorage.setItem('counterMinValue', state.minValue.toString()), [state.minValue])
-        useEffect(() => localStorage.setItem('counterMaxValue', state.maxValue.toString()), [state.maxValue])
-        useEffect(() => localStorage.setItem('counterValue', state.value.toString()), [state.value])
-
-        const inputMinChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
-            dispatchState(minTitleChangeAC(e.currentTarget.value));
-        }
-        const inputMaxChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
-            dispatchState(maxTitleChangeAC(e.currentTarget.value));
-        }
-        const setHandler = () => {
-            dispatchState({
-                type: 'SET_HANDLER'
-            });
-        }
-        const incHandler = () => {
-            dispatchState({
-                type: 'INC_HANDLER',
-            });
-        }
-        const resetHandler = () => {
-            localStorage.removeItem('counterValue') // удаление сохраненного значения счетчика
-            dispatchState(resetAC());
-        }
-        return (
-            <div className={s.App}>
-                <Logo/>
-                <div className={s.body}>
-                    <Set setHandler={setHandler} inputMinChangeHandler={inputMinChangeHandler}
-                         inputMaxChangeHandler={inputMaxChangeHandler} inputMinTitle={state.inputMinTitle}
-                         inputMaxTitle={state.inputMaxTitle} error={state.error} /*onMouseOver={onMouseOver}*/
-                         disable={state.disable} /*onMouseOut={onMouseOut}*//>
-                    <Counter value={state.value} resetHandler={resetHandler} error={state.error}
-                             incHandler={incHandler} storageMinValueAsString={storageMinValueAsString}
-                             storageMaxValueAsString={storageMaxValueAsString}/>
-                </div>
-            </div>
-        );
+    const storageMinValueAsString = localStorage.getItem('counterMinValue');
+    const storageMinValue = storageMinValueAsString ? +storageMinValueAsString : 0;
+    const storageMaxValueAsString = localStorage.getItem('counterMaxValue');
+    const storageMaxValue = storageMaxValueAsString ? +storageMaxValueAsString : 10;
+    const storageValueAsString = localStorage.getItem('counterValue');
+    const initialState: StateType = {
+        value: storageValueAsString && storageMinValueAsString ?
+            Math.max(+storageValueAsString, +storageMinValueAsString) : storageValueAsString ?
+                +storageValueAsString : storageMinValueAsString ? +storageMinValueAsString : 0,
+        minValue: storageMinValue,
+        maxValue: storageMaxValue,
+        error: storageValueAsString && storageMaxValueAsString && (+storageValueAsString >=
+            +storageMaxValueAsString) ? storageValueAsString : '',
+        inputMinTitle: storageMinValue,
+        inputMaxTitle: storageMaxValue,
+        disable: true
     }
+    const [state, dispatchState] = useReducer(reducer, initialState)
 
-    export default App;
+    useEffect(() => localStorage.setItem('counterMinValue', state.minValue.toString()), [state.minValue])
+    useEffect(() => localStorage.setItem('counterMaxValue', state.maxValue.toString()), [state.maxValue])
+    useEffect(() => localStorage.setItem('counterValue', state.value.toString()), [state.value])
+
+    const inputMinChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        dispatchState(minTitleChangeAC(e.currentTarget.value));
+    }
+    const inputMaxChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        dispatchState(maxTitleChangeAC(e.currentTarget.value));
+    }
+    const setHandler = () => {
+        dispatchState({
+            type: 'SET_HANDLER'
+        });
+    }
+    const incHandler = () => {
+        dispatchState({
+            type: 'INC_HANDLER',
+        });
+    }
+    const resetHandler = () => {
+        localStorage.removeItem('counterValue') // удаление сохраненного значения счетчика
+        dispatchState(resetAC());
+    }
+    return (
+        <div className={s.App}>
+            <Logo/>
+            <div className={s.body}>
+                <Set setHandler={setHandler} inputMinChangeHandler={inputMinChangeHandler}
+                     inputMaxChangeHandler={inputMaxChangeHandler} inputMinTitle={state.inputMinTitle}
+                     inputMaxTitle={state.inputMaxTitle} error={state.error}
+                     disable={state.disable}/>
+                <Counter value={state.value} resetHandler={resetHandler} error={state.error}
+                         incHandler={incHandler} storageMinValueAsString={storageMinValueAsString}
+                         storageMaxValueAsString={storageMaxValueAsString}/>
+            </div>
+        </div>
+    );
+}
+
+export default App;
